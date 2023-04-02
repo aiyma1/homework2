df306ea28ad70a8d352ed19b5a86663e8735f737
aiyma <126954094+aiyma1@users.noreply.github.com>
2023-04-02 15:01:57 +0600

    Add files via upload

---
+++ form/script.js
@@ -0,0 +1,22 @@
+// let input = document.getElementById('text')
+          
+// text.addEventListener('change', (e) => {
+//     localStorage.setItem('key', text.value)
+// })
+
+// let text = document.getElementById('textIn')
+// localStorage.setItem('key', text);
+// let value = localStorage.getItem('key');
+
+// localStorage.('key'); 
+// localStorage.clear();
+
+
+
+// let input =  document.getElementById('textIn')
+// localStorage.setItem('key', textValue)
+// // let value = localStorage.getItem('key');
+// let textValue = JSON.parse(localStorage.getItem('key'));
+// console.log(input);
+// localStorage.clear()
+
